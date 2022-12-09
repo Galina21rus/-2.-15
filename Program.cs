f71cc07b480c54d6db21611344e73492ce2b3bfb
galina21rus <galina21rus.sensum@gmail.com>
2022-12-09 14:07:49 +0300

    fix

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿int number = ReadInt ("Введите число дня недели от 1 до 7: ")
+
+// if number < 1 or number > 7: print('Вы ввели неверное число')
+// if number > 5: print('День выходной!')
+// else: print('Увы, но это рабочий день!')
